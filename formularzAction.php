d4f49c38700a7ec5c3779435d19ee16366a92b19
bkmiecik12 <bkmiecik12@gmail.com>
2017-11-25 13:25:02 +0100

    PHP_1

---
+++ formularzAction.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <meta charset="utf-8">
+
+  <title>Recenzje filmów - Walidacja formularza</title>
+  <meta name="description" content="Recenzje filmów">
+  <meta name="keywords" content="Film, ranking, recenzja, premiery">
+  <link rel = "stylesheet" type = "text/css" href = "style/styles.css">
+  
+  <script src="js/dom3.js"></script>
+
+  <!--aaa-->
+</head>
+
+<body>
+<div id='topmain'>
+  <h1>Recenzje filmów</h1>
+  <p>Walidacja formularza</p>
+</div>
+  <nav>
+  <ul>
+  <li><a href="index.html">Start</a></li>
+  <li><a href="ranking.html">Ranking filmów</a></li>
+  <li><a href="najnowsze.html">Najnowsze recenzje</a></li>
+  <li><a href="formularz.html">Dołącz do grona kinomaniaków !</a></li>
+	</ul>
+	</nav>
+
+  Kolor tła:<select onchange="changeBackground(this);" id="tlo" name="tlo">
+	<option selected>#ddd</option>
+    <option>lightblue</option>
+	<option>yellow</option>
+	<option>white</option>
+  </select>
+  Kolor czcionki:<select onchange="changeText(this);" id="tekst" name="tekst">
+	<option selected>black</option>
+    <option>red</option>
+	<option>yellow</option>
+	<option>green</option>
+  </select>
+  Rodzaj czcionki:<select onchange="changeFont(this);" id="czcionka" name="czcionka">
+	<option selected>Times New Roman</option>
+    <option>Arial</option>
+	<option>Impact</option>
+	<option>Courier New</option>
+  </select>
+  
+<p>Twoje imię: 
+<?php 
+	$name = $_POST["name"];
+	if(strlen($name)>20) {
+		die("Za duża liczba znaków dla imienia !");
+	}
+	else {
+		print($name);
+	}
+?>
+</p>
+<p>Twoje nazwisko: 
+<?php 
+	$nazwisko = $_POST["nazwisko"];
+	if(strlen($nazwisko)>20) {
+		die("Za duża liczba znaków dla nazwiska !");
+	}
+	else {
+		print($nazwisko);
+	}
+?>
+</p>
+<p>Twój login: 
+<?php 
+	define("SEP","-");
+	$los=rand(1,50);
+	$login= $_POST["name"].SEP.$_POST["nazwisko"];
+	$los+=strlen($login);
+	settype($los,"string");
+	echo $login, SEP, (string)$los
+?>
+</p>
+<p>Pora, w jakiej się urodziłeś: 
+<?php 
+	$miesiac=$_POST["miesiac"];	
+	switch($miesiac) {
+	case "Styczen":
+		echo("Zima");
+		break;
+	case "Luty":
+		echo("Zima");
+		break;
+	case "Marzec":
+		echo("Wiosna");
+		break;
+	case "Kwiecen":
+		echo("Wiosna");
+		break;
+	case "Maj":
+		echo("Wiosna");
+		break;
+	case "Czerwiec":
+		echo("Lato");
+		break;
+	case "Lipiec":
+		echo("Lato");
+		break;
+	case "Sierpien":
+		echo("Lato");
+		break;
+	case "Wrzesien":
+		echo("Jesien");
+		break;
+	case "Pazdziernik":
+		echo("Jesien");
+		break;
+	case "Listopad":
+		echo("Jesien");
+		break;
+	case "Grudzien":
+		echo("Zima");
+		break;
+	default:
+		echo("Nie podałeś nam miesiąca !");
+	}
+?>
+</p>
+<p>Polecane filmy na pory roku: 
+<?php 
+	$map = ["Zima" => "Kevin sam w domu",
+	"Wiosna" => "Nowy rok, nowy Ty",
+	"Lato" => "Wakacje żegnaj szkoło",
+	"Jesień" => "Kiedy opadają liście"];
+	foreach($map as $key=>$value) {
+		echo "<br>",$key, " => ", $value;
+	}
+?>
+</p>  
+<p>Domena maila: 
+<?php 
+	$email=$_POST["email"];
+	for ($i = 0; $i < strlen($email); $i++){
+		if($email[$i]=="@") {
+			echo substr($email,$i+1,strlen($email));
+			break;
+		}
+	}
+	if(substr($email,strlen($email)-2,strlen($email))=="pl") {
+		echo " => \"Polska\" domena !";
+	}
+	else {
+		echo " => \"Obca\" domena !";
+	}
+?>
+</p>
+<p>Podany numer telefonu: 
+<?php 
+	$telefon=$_POST["telefon"];
+	if (preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{3}$/",$telefon)) {
+		echo preg_replace("/-/"," ",$telefon);
+	}
+?>
+</p>
+<p>Wybrane kultowe filmy: 
+<?php 
+	
+	$cult = $_POST['cult'];
+	if(count($cult)<2) {
+		print("Nie wybrałeś 2 kultowych filmów !");
+	}
+	else {
+		if(count($cult)==5) {
+			print("Ooo, prawdziwy z Ciebie kinoman !: ");
+		}
+		foreach($cult AS $value) {
+			echo $value.", ";
+		}
+	}
+?>
+</p>
+<p>URL formularza: 
+<?php	
+	echo $_SERVER['HTTP_REFERER'];
+?>
+</p>
+</body>
+</html>
--- newreview.php
+++ newreview.php
@@ -1,34 +1,80 @@
 <!DOCTYPE HTML>
 <html>
     <head>
+    <style type="text/css">
+    body{
+        background:#0a26fb;
+        text-align: center;
+    }
+    .error{
+        text-align: center;
+        color: red;
+        width: 30%;
+        background-color:#eaf2ff;
+        margin: auto auto;
+        padding:10px;
+    }
+    .confirm{
+        text-align: left;
+        width: 30%;
+        background-color:#eaf2ff;
+        margin: auto auto;
+        padding: 10px;
+    }
+    
+    </style>
 
     <?php
-        $filmy['dramat'] = ["8 mila",""];
+        $filmy['Dramat'] = ["8 mila","Nietykalni","Dzień Świra"];
+        $filmy['Komedia'] = ["American Pie","Kiler","Kac Vegas"];
+        $filmy['Horror'] = ["To","Paranormal Activity","Egzorcyzmy Emily Rose"];
+        $filmy['Thriller'] = ["Se7en","Milczenie owiec","Podziemny krąg"];
+        $filmy['Film akcji'] = ["Fast and Furious","Uprowadzona","Transporter"];
+        $filmy['Familijny'] = ["Epoka Lodowcowa","Shrek","Kevin sam w domu"];
+
+
+        $user = $_POST['user'];
+        $review = $_POST['review'];
+        $title = $_POST['title'];
     ?>
 
     <?php
-
-            $user = $_POST['user'];
-            $review = $_POST['review'];
-            $title = $_POST['title'];
-            
-            if(!preg_match('/[[:alnum:]]/',$user)){
-                print('<title>Nieprawidłowa nazwa użytkownika</title>
-                </head>
+            define("EMPTY",0);
+    
+            function headNshoulders($error){
+                print('<title>'.$error.'</title></head>
                 <body>
-                <div class=error>
-                    Nieprawidłowa nazwa użytkownika<br>
-                    <button onclick="goBack()">Powrót do formularza</button>
+                <div class=error>'
+                    .$error.'<br>');
+            }
+
+            function endOfPage(){
+                print('<button onclick="goBack()">Powrót do formularza</button>
                 
                         <script>
                             function goBack() {
                                 window.history.back();
                             }
                         </script>
-                <div>
+                </div>
                 </body>
                 </html>');
                 die();
+            }
+
+
+            if(!preg_match('/[[:alnum:]]/',$user)){
+                headNshoulders('Nieprawidłowa nazwa użytkownika');
+                endOfPage();
+            }
+            
+            else if(strlen($user)=="EMPTY" || strlen($title)=="EMPTY" || strlen($review)=="EMPTY"){
+                headNshoulders('Uzupełnij wszystkie pola');
+                endOfPage();
+            }
+            else if(!isset($_POST['category'])){
+                headNshoulders('Wybierz kategorie');
+                endOfPage();
             }
         ?>
         <title>Dodano nową recezję</title>
@@ -36,19 +82,27 @@
     <body>
         <div class="confirm">
             <?php
-                echo("Użytkowniku <b><i>".$user."</i></b>! Twoja recenzja została dodana!<br />");
+                echo("Użytkowniku <b><i>".$user."</i></b>! Twoja recenzja została dodana!<br /><br />");
                 echo("Tytuł filmu: <b>".$title."</b><br />");
                 echo('Treść recenzji: '.$review)."<br />";
                 echo('Użytych znaków: '.strlen($review)."<br />");
+                echo('Wybrane kategorie: <br />');
                 
                 $category = $_POST['category'];
 
+
                 foreach ($category as $name){ 
-                    echo $name."<br />";
+                    echo "<b>".$name."</b><br />";
                 }
-
+                echo "<br />Spośród dostępnych: <br />";
+                for(reset($filmy) ; $element = key($filmy) ; next($filmy)){
+                    echo "<i>".$element."</i> | ";
+                }
                 
-
+                echo('<br /><br /><b>Propozycje dla Ciebie:</b><br />');
+                for($i=0;$i<3;$i++){
+                    echo($filmy[$category[$i%count($category)]][$i].'<br />');
+                }
             ?>
         </div>
     </body>
